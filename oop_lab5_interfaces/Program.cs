7ae5b7430c65d3c35b9704f461ce01b351793b86
lb <bozhko.olena1123@vu.cdu.edu.ua>
2024-12-24 23:20:04 +0200

    Initial commit

---
+++ oop_lab5_interfaces/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_lab5_interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+        }
+    }
+}
