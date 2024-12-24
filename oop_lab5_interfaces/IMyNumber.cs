beb2911f2f04c717bd4eb4555096f2ee2af1d1b5
lb <bozhko.olena1123@vu.cdu.edu.ua>
2024-12-24 23:25:41 +0200

    Done IMyNumber interface as the guide said, Done MyFrac Class that inheritates IMyNumber, tested it using TestAPlusBSquare method from the guide

---
+++ oop_lab5_interfaces/IMyNumber.cs
@@ -0,0 +1,10 @@
+﻿namespace oop_lab5_interfaces
+{
+    public interface IMyNumber<T> where T : IMyNumber<T>
+    {
+        T Add(T b);
+        T Subtract(T b);
+        T Multiply(T b);
+        T Divide(T b);
+    }
+}
